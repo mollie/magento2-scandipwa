1e5e5f55fa00c1941648df9d1da0cb7d3a9af9c5
Michiel Gerritsen <michiel@controlaltdelete.nl>
2024-06-24 13:21:19 +0200

    Feature: Added methods

--- src/plugin/CheckoutPaymentsComponent.plugin.js
+++ src/plugin/CheckoutPaymentsComponent.plugin.js
@@ -4,10 +4,14 @@ const mollieIssuerRender = (method) => () => <MollieIssuer method={ method } />;
 
 const paymentRenderMap = (originalMember) => ({
     ...originalMember,
+    mollie_methods_alma: mollieIssuerRender('mollie_methods_alma').bind(this),
     mollie_methods_applepay: mollieIssuerRender('mollie_methods_applepay').bind(this),
     mollie_methods_bancontact: mollieIssuerRender('mollie_methods_bancontact').bind(this),
+    mollie_methods_bancomatpay: mollieIssuerRender('mollie_methods_bancomatpay').bind(this),
     mollie_methods_banktransfer: mollieIssuerRender('mollie_methods_banktransfer').bind(this),
     mollie_methods_belfius: mollieIssuerRender('mollie_methods_belfius').bind(this),
+    mollie_methods_billie: mollieIssuerRender('mollie_methods_billie').bind(this),
+    mollie_methods_blik: mollieIssuerRender('mollie_methods_blik').bind(this),
     mollie_methods_creditcard: mollieIssuerRender('mollie_methods_creditcard').bind(this),
     mollie_methods_directdebit: mollieIssuerRender('mollie_methods_directdebit').bind(this),
     mollie_methods_eps: mollieIssuerRender('mollie_methods_eps').bind(this),
@@ -16,16 +20,18 @@ const paymentRenderMap = (originalMember) => ({
     mollie_methods_ideal: mollieIssuerRender('mollie_methods_ideal').bind(this),
     mollie_methods_in3: mollieIssuerRender('mollie_methods_in3').bind(this),
     mollie_methods_kbc: mollieIssuerRender('mollie_methods_kbc').bind(this),
+    mollie_methods_klarna: mollieIssuerRender('mollie_methods_klarna').bind(this),
     mollie_methods_klarnapaylater: mollieIssuerRender('mollie_methods_klarnapaylater').bind(this),
     mollie_methods_klarnapaynow: mollieIssuerRender('mollie_methods_klarnapaynow').bind(this),
     mollie_methods_klarnasliceit: mollieIssuerRender('mollie_methods_klarnasliceit').bind(this),
-    mollie_methods_voucher: mollieIssuerRender('mollie_methods_voucher').bind(this),
     mollie_methods_mybank: mollieIssuerRender('mollie_methods_mybank').bind(this),
     mollie_methods_paypal: mollieIssuerRender('mollie_methods_paypal').bind(this),
     mollie_methods_paysafecard: mollieIssuerRender('mollie_methods_paysafecard').bind(this),
     mollie_methods_przelewy24: mollieIssuerRender('mollie_methods_przelewy24').bind(this),
     mollie_methods_sofort: mollieIssuerRender('mollie_methods_sofort').bind(this),
-    mollie_methods_twint: mollieIssuerRender('mollie_methods_twint').bind(this)
+    mollie_methods_riverty: mollieIssuerRender('mollie_methods_sofort').bind(this),
+    mollie_methods_twint: mollieIssuerRender('mollie_methods_twint').bind(this),
+    mollie_methods_voucher: mollieIssuerRender('mollie_methods_voucher').bind(this),
 });
 
 export default {
--- src/util/MollieSupportedMethods.js
+++ src/util/MollieSupportedMethods.js
@@ -1,8 +1,12 @@
 export const MOLLIE_METHODS = [
+    'mollie_methods_alma',
     'mollie_methods_applepay',
     'mollie_methods_bancontact',
+    'mollie_methods_bancomatpay',
     'mollie_methods_banktransfer',
     'mollie_methods_belfius',
+    'mollie_methods_billie',
+    'mollie_methods_blik',
     'mollie_methods_creditcard',
     'mollie_methods_directdebit',
     'mollie_methods_eps',
@@ -11,16 +15,18 @@ export const MOLLIE_METHODS = [
     'mollie_methods_ideal',
     'mollie_methods_in3',
     'mollie_methods_kbc',
+    'mollie_methods_klarna',
     'mollie_methods_klarnapaylater',
     'mollie_methods_klarnapaynow',
     'mollie_methods_klarnasliceit',
-    'mollie_methods_voucher',
     'mollie_methods_mybank',
     'mollie_methods_paypal',
     'mollie_methods_paysafecard',
     'mollie_methods_przelewy24',
     'mollie_methods_sofort',
-    'mollie_methods_twint'
+    'mollie_methods_riverty',
+    'mollie_methods_twint',
+    'mollie_methods_voucher',
 ];
 
 export default MOLLIE_METHODS;
